f74ddab367d4e4456aa655efeb45b6d18f93213d
sky <anvanc37@gmail.com>
2022-01-04 22:48:05 -0500

    finish the project

--- Clima/Clima/Controller/WeatherViewController.swift
+++ Clima/Clima/Controller/WeatherViewController.swift
@@ -6,20 +6,46 @@
 //
 
 import UIKit
+import CoreLocation
 
-class WeatherViewController: UIViewController, UITextFieldDelegate {
+class WeatherViewController: UIViewController {
     
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var searchTextField: UITextField!
+    
+    var weatherManager = WeatherManager()
+    var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // ask permission to get access user's location. it will show popup alert to user
+        // add 1 line in plist file -> App Category -> Privacy - Location When In Use Usage Description
+        
+        // locationManager delegate has to assign to self BEFORE requestWhenInUseAuthorization. Otherwise, app will crash
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        // when we call requestLocation, the func didUpdateLocations in extension will be triggered
+        locationManager.requestLocation()
+        
+        weatherManager.delegate = self
         searchTextField.delegate = self
+        
     }
+    
+    @IBAction func locationButtonPressed(_ sender: UIButton) {
+        // khi minh call requestLocation func, the func didUpdateLocations in extension will be triggered
+        locationManager.requestLocation()
+    }
+    
+}
 
+//MARK: - UITextFieldDelegate
+
+extension WeatherViewController: UITextFieldDelegate {
+    
     @IBAction func searchPressed(_ sender: UIButton) {
         searchTextField.endEditing(true)
         print(searchTextField.text!)
@@ -36,6 +62,9 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     // when user finish editing the textField. it will work for both searchPressed and textFiledShouldReturn.
     // instead of reset text into empty string in both funcs above, we reset it here to avoid repeat code
     func textFieldDidEndEditing(_ textField: UITextField) {
+        if let city = searchTextField.text {
+            weatherManager.fetchWeather(cityName: city)
+        }
         searchTextField.text = ""
         searchTextField.placeholder = "Search"
     }
@@ -53,3 +82,39 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     }
 }
 
+// MARK: - WeatherManagerDelegate
+
+extension WeatherViewController: WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+            self.cityLabel.text = weather.cityName
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error.localizedDescription)
+    }
+}
+
+// MARK: - CCLocationManagerDelegate
+// when we implement CLLocationManagerDelegate, we have to implement 2 func, didUpdateLocations va didFailWithError
+
+extension WeatherViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // locations has an array of lots of locations -> choose the last one would be the most accurate one
+        if let location = locations.last {
+            // when we already get location, we need to stop updating location
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+        }
+        print("Got location data")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
---
+++ Clima/Clima/Model/WeatherModel.swift
@@ -0,0 +1,37 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by sky on 1/4/22.
+//
+
+import Foundation
+
+struct WeatherModel {
+    let conditionId: Int
+    let cityName: String
+    let temperature: Double
+    
+    var temperatureString: String {
+        return String(format: "%.1f", temperature)
+    }
+    
+    var conditionName: String {
+        switch conditionId {
+        case 200...232:
+            return "cloud.bolt"
+        case 300...312:
+            return "cloud.drizzle"
+        case 500...531:
+            return "cloud.rain"
+        case 701...781:
+            return "cloud.fog"
+        case 800:
+            return "sun.max"
+        case 801...804:
+            return "cloud.bolt"
+        default:
+            return "cloud"
+        }
+    }
+}
